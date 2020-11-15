9f9300b3b7fbcc8efee4036c968d66636ea097ca
Nathan Pastor <npastor24@biomedscienceacademy.org>
2020-11-15 13:17:12 +0000

    I did some revision and worked on some variables, I also forgot to commit & push the first time.

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Nathan Pastor
+Period/Core: 5
 
 
 """
+print("Let's play " + "Silly Sentences!")
+#The Variables that I use
+name_friend = input("Enter a name: ")
+greeting = input("Write a greeting: ")
+subject = input("Enter a subject to talk about: ")
+animal = input("Enter the name of an animal: ")
+noun = input("Enter a noun: ")
+movie_name = input("Enter a movie: ")
+feeling = input("Enter a feeling: ")
+#The Variables that I use
+print(f"One day as I was walking home from school I ran into my old friend, {name_friend} it had been years since I've seen them. I walked over to them and I said, \n{greeting} \n{name_friend} looked at me, \"Hello\" {name_friend} said, \"How've you been\" \n\"good,\" I said back \n we started to walk back towards my house and started to talk about {subject}. As we were walking back to my house we saw a {animal}, we started to walk towards the {animal} and once we got to it we started to pet it. After petting the {animal} seemed to get a little aggitated and tried to bite at {name_friend}. {name_friend} looked startled and stumbled backwards, \n\"Let's go\" {name_friend} said. \nAs we contiuned our walk home a {noun} came up to us and asked if we wanted to see the new movie, {movie_name} and if so he has tickets he can sell us. I looked at, {name_friend} and they looked at me. I looked at the {noun} and said, \"No thanks maybe another time\" and we walked away. \n Once we reached my house we said our goodbyes and I went inside. I went straight to my room and went to bed because of how {feeling} I was from my day")
